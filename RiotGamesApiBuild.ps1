e5f64e85534448cf39360e7b84cebe976e6043a9
msx752 <aslim@outlook.com.tr>
2017-07-14 16:36:18 +0300

    minor change

--- RiotGamesApiBuild.ps1
+++ RiotGamesApiBuild.ps1
@@ -2,11 +2,13 @@
 clear
 $firstCommit = $(git log --pretty=format:'%H' -n -1).Length;
 $firstCommitHash = $(git log --pretty=format:'%H' -n -1)[$firstCommit-1];
+Write-Host "First Commit Hash:" $firstCommitHash;
 $firstCommitDate = (git show -s --format=%ci $firstCommitHash);
+Write-Host "First Commit Date:" $firstCommitDate;
 [datetime]$FCDateTime = $firstCommitDate;
-Write-Host $FCDateTime
+#Write-Host $FCDateTime
 $FCyear = $FCDateTime.ToString("y.").TrimEnd('.');
-Write-Host $FCyear
+#Write-Host $FCyear
 $timeZone0 = [TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([DateTime]::UtcNow, "Turkey Standard Time");
 $timeZone = $timeZone0;
 [datetime]$DateTime = $timeZone;
@@ -19,7 +21,7 @@ $buildYear = (($year - $FCyear) + 1);
 #$buildTime = $DateTime.ToString("M.d.hmm");
 $newBuildNumber = "$buildYear.$month.$day.$hourMinute";
 
-Write-Host $newBuildNumber
+Write-Host "New Nuget Package Version:" $newBuildNumber
 
 dotnet clean -c Release
 dotnet build -c Release  /p:Version=$newBuildNumber --framework netcoreapp1.1
