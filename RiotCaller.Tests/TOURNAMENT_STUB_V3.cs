3a992c787d2803a6f4a9988e79695f389155529c
msx752 <aslim@outlook.com.tr>
2017-07-17 23:37:41 +0300

    tournamenapi

---
+++ RiotCaller.Tests/TOURNAMENT_STUB_V3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RiotGamesApi.AspNetCore.Enums;
+using RiotGamesApi.AspNetCore.Models;
+using RiotGamesApi.AspNetCore.RiotApi.NonStaticEndPoints.ChampionMastery;
+using RiotGamesApi.AspNetCore.RiotApi.TournamentStubEndPoints;
+using Xunit;
+
+namespace RiotGamesApi.Tests
+{
+    public class TOURNAMENT_STUB_V3 : BaseTestClass
+    {
+        [Fact]
+        public void GetLobbyEvents()
+        {
+            var rit = new ApiCall()
+                .SelectApi<LobbyEventDTOWrapper>(ApiName.TournamentStub)
+                .For(ApiMethodName.Codes)
+                .AddParameter()
+                .Build(PlatformType)
+                .Get();
+            Assert.False(rit.HasError);
+        }
+    }
+}
