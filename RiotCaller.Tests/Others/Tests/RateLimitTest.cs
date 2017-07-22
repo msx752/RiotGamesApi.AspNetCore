180613010d3e4ba8d6883618bbde6e837585b475
msx752 <aslim@outlook.com.tr>
2017-07-23 01:38:01 +0300

    RateLimiting is added

--- RiotCaller.Tests/Others/Tests/RateLimitTest.cs
+++ RiotCaller.Tests/Others/Tests/RateLimitTest.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using RiotGamesApi.AspNetCore.Interfaces;
 using RiotGamesApi.AspNetCore.Models;
+using RiotGamesApi.AspNetCore.RateLimit;
 using RiotGamesApi.AspNetCore.RiotApi.StaticEndPoints.Champions;
 using Xunit;
 
 namespace RiotGamesApi.Tests.Others
 {
-    public class RateLimiter : BaseTestClass
+    public class RateLimitTest : BaseTestClass
     {
         [Fact]
-        public void RateLimiting()//debug mode
+        public void RateLimiting()//on debug mode
         {
             for (int i = 0; i < 205; i++)
             {
@@ -17,6 +20,20 @@ namespace RiotGamesApi.Tests.Others
                     LolApi.StaticApi.StaticDatav3.GetChampionsOnlyId(AspNetCore.RiotApi.Enums.ServicePlatform.TR1, 45, true);
                 Assert.False(result.HasError);
             }
+        }
+
+        [Fact]
+        public void RateLimit2()//on debug mode
+        {
+            ApiRate rt = new ApiRate();
+            for (int i = 0; i < 205; i++)
+            {
+                Task.Run(() =>
+                {
+                    rt.Handle();
+                });
+            }
+            Console.ReadLine();
         }
     }
 }
