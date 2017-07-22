a0bd77fc10a9e893f8c74479c2027d531ed00242
msx752 <aslim@outlook.com.tr>
2017-07-22 21:01:25 +0300

    r

--- RiotCaller.AspNetCore/RateLimit/RateLimitOption.cs
+++ RiotCaller.AspNetCore/RateLimit/RateLimitOption.cs
@@ -1,40 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RiotGamesApi.AspNetCore.RateLimit
 {
     public class RateLimitOption
     {
-        public Dictionary<TimeSpan, int> Limits { get; set; } =
-            new Dictionary<TimeSpan, int>();
+        public List<RLimit> RateLimits { get; private set; } = new List<RLimit>();
 
         public RateLimitOption AddDays(short multiplier, int limit)
         {
-            AddTimeSpan(new TimeSpan(multiplier, 0, 0, 0), limit);
+            Add(new TimeSpan(multiplier, 0, 0, 0), limit);
             return this;
         }
 
         public RateLimitOption AddHours(short multiplier, int limit)
         {
-            Limits[new TimeSpan(multiplier, 0, 0)] = limit;
+            Add(new TimeSpan(multiplier, 0, 0), limit);
             return this;
         }
 
         public RateLimitOption AddMinutes(short multiplier, int limit)
         {
-            AddTimeSpan(new TimeSpan(0, multiplier, 0), limit);
+            Add(new TimeSpan(0, multiplier, 0), limit);
             return this;
         }
 
         public RateLimitOption AddSeconds(short multiplier, int limit)
         {
-            AddTimeSpan(new TimeSpan(0, 0, multiplier), limit);
+            Add(new TimeSpan(0, 0, multiplier), limit);
             return this;
         }
 
-        private void AddTimeSpan(TimeSpan ts, int limit)
+        private void Add(TimeSpan ts, int limit)
         {
-            Limits[ts] = limit;
+            RateLimits.Add(new RLimit(ts, limit));
+            RateLimits = RateLimits.OrderByDescending(p => p.Time).ToList();
         }
     }
 }
