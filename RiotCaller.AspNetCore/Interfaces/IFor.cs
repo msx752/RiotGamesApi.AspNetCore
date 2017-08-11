c8b24b88e140c54b1090cd1f922c05e152d04432
msx752 <aslim@outlook.com.tr>
2017-08-12 00:10:07 +0300

    project moved to msx752/RiotGamesApi

--- RiotCaller.AspNetCore/Interfaces/IFor.cs
+++
@@ -1,6 +0,0 @@
-using RiotGamesApi.AspNetCore.Enums;
-
-namespace RiotGamesApi.AspNetCore.Interfaces
-{
-    public interface IFor<T> where T : new() { IAddParameter<T> For(LolApiMethodName middleType); }
-}
--- RiotCaller.AspNetCore/Interfaces/IRSelectApi.cs
+++
@@ -1,6 +0,0 @@
-using RiotGamesApi.AspNetCore.Enums;
-
-namespace RiotGamesApi.AspNetCore.Interfaces
-{
-    public interface IRSelectApi<T> where T : new() { IFor<T> SelectApi(LolApiName apiType, double version = 3.0); }
-}
